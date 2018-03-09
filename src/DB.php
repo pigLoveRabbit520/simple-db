5c96fbd1fc57c743215a67cf1c651ab225fd649c
salamander-mh <1906747819@qq.com>
2018-03-09 10:31:35 +0800

    初始化项目

---
+++ src/DB.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * User: salamander
+ * Date: 2016/9/2
+ * Time: 9:16
+ */
+namespace SimpleDB;
+
+use SimpleDB\MySQLException;
+
+class DB
+{
+    private $dsn;
+    private $sth;
+    /**
+     * @var \PDO $dbh
+     */
+    private $dbh;
+    private $user;
+    private $charset;
+    private $password;
+
+    public $lastSQL = '';
+
+    public function __setup($config = array())
+    {
+        $this->dsn = $config['dsn'];
+        $this->user = $config['username'];
+        $this->password = $config['password'];
+        $this->charset = $config['charset'];
+        $this->connect();
+    }
+
+    private function connect()
+    {
+        if(!$this->dbh) {
+            $options = array(
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES ' . $this->charset,
+            );
+            $this->dbh = new \PDO($this->dsn, $this->user,
+                $this->password, $options);
+        }
+    }
+
+    public function beginTransaction()
+    {
+        return $this->dbh->beginTransaction();
+    }
+
+    public function inTransaction()
+    {
+        return $this->dbh->inTransaction();
+    }
+
+    public function rollBack()
+    {
+        return $this->dbh->rollBack();
+    }
+
+    public function commit()
+    {
+        return $this->dbh->commit();
+    }
+
+    function watchException($execute_state)
+    {
+        if(!$execute_state){
+            throw new MySQLException("SQL: {$this->lastSQL}\n".$this->sth->errorInfo()[2], intval($this->sth->errorCode()));
+        }
+    }
+
+    /**
+     * @param $sql
+     * @param array $parameters
+     * @return array
+     * @throws MySQLException
+     */
+    public function fetchAll($sql, $parameters=[])
+    {
+        $result = [];
+        $this->lastSQL = $sql;
+        $this->sth = $this->dbh->prepare($sql);
+        $this->watchException($this->sth->execute($parameters));
+        while($result[] = $this->sth->fetch(\PDO::FETCH_ASSOC)){ }
+        array_pop($result);
+        return $result;
+    }
+
+    /**
+     * @param $sql
+     * @param array $parameters
+     * @param int $position
+     * @return array
+     * @throws MySQLException
+     */
+    public function fetchColumnAll($sql, $parameters=[], $position=0)
+    {
+        $result = [];
+        $this->lastSQL = $sql;
+        $this->sth = $this->dbh->prepare($sql);
+        $this->watchException($this->sth->execute($parameters));
+        while($result[] = $this->sth->fetch(\PDO::FETCH_COLUMN, $position)){ }
+        array_pop($result);
+        return $result;
+    }
+
+    public function exists($sql, $parameters=[])
+    {
+        $this->lastSQL = $sql;
+        $data = $this->fetch($sql, $parameters);
+        return !empty($data);
+    }
+
+    /**
+     * @param string $sql
+     * @param array $parameters
+     * @return int
+     * @throws MySQLException
+     */
+    public function query($sql, $parameters=[])
+    {
+        $this->lastSQL = $sql;
+        $this->sth = $this->dbh->prepare($sql);
+        $this->watchException($this->sth->execute($parameters));
+        return $this->sth->rowCount();
+    }
+
+    /**
+     * @param $sql
+     * @param array $parameters
+     * @param int $type
+     * @return mixed
+     * @throws MySQLException
+     */
+    public function fetch($sql, $parameters=[], $type=\PDO::FETCH_ASSOC)
+    {
+        $this->lastSQL = $sql;
+        $this->sth = $this->dbh->prepare($sql);
+        $this->watchException($this->sth->execute($parameters));
+        return $this->sth->fetch($type);
+    }
+
+    /**
+     * @param $sql
+     * @param array $parameters
+     * @param int $position
+     * @return mixed
+     * @throws MySQLException
+     */
+    public function fetchColumn($sql, $parameters=[], $position=0)
+    {
+        $this->lastSQL = $sql;
+        $this->sth = $this->dbh->prepare($sql);
+        $this->watchException($this->sth->execute($parameters));
+        return $this->sth->fetch(\PDO::FETCH_COLUMN, $position);
+    }
+
+    public function update($table, $parameters=[], $condition=[])
+    {
+        $table = $this->format_table_name($table);
+        $sql = "UPDATE $table SET ";
+        $fields = [];
+        $pdo_parameters = [];
+        foreach ( $parameters as $field=>$value){
+            $fields[] = '`'.$field.'`=:field_'.$field;
+            $pdo_parameters['field_'.$field] = $value;
+        }
+        $sql .= implode(',', $fields);
+        $fields = [];
+        $where = '';
+        if(is_string($condition)) {
+            $where = $condition;
+        } else if(is_array($condition)) {
+            foreach($condition as $field=>$value){
+                $parameters[$field] = $value;
+                $fields[] = '`'.$field.'`=:condition_'.$field;
+                $pdo_parameters['condition_'.$field] = $value;
+            }
+            $where = implode(' AND ', $fields);
+        }
+        if(!empty($where)) {
+            $sql .= ' WHERE '.$where;
+        }
+        return $this->query($sql, $pdo_parameters);
+    }
+
+    /**
+     * @param string $table
+     * @param array $parameters
+     * @return int|string
+     * @throws MySQLException
+     */
+    public function insert($table, $parameters=[])
+    {
+        $table = $this->format_table_name($table);
+        $sql = "INSERT INTO $table";
+        $fields = [];
+        $placeholder = [];
+        foreach ( $parameters as $field=>$value){
+            $placeholder[] = ':'.$field;
+            $fields[] = '`'.$field.'`';
+        }
+        $sql .= '('.implode(",", $fields).') VALUES ('.implode(",", $placeholder).')';
+
+        $this->lastSQL = $sql;
+        $this->sth = $this->dbh->prepare($sql);
+        $this->watchException($this->sth->execute($parameters));
+        $id = $this->dbh->lastInsertId();
+        if(empty($id)) {
+            return $this->sth->rowCount();
+        } else {
+            return $id;
+        }
+    }
+
+    public function errorInfo()
+    {
+        return $this->sth->errorInfo();
+    }
+
+    public function getDBH()
+    {
+        return $this->dbh;
+    }
+
+    protected function format_table_name($table)
+    {
+        $parts = explode(".", $table, 2);
+
+        if(count($parts) > 1) {
+            $table = $parts[0].".`{$parts[1]}`";
+        } else {
+            $table = "`$table`";
+        }
+        return $table;
+    }
+
+    function errorCode()
+    {
+        return $this->sth->errorCode();
+    }
+}
---
+++ src/Exception/MySQLException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: salamander
+ * Date: 2017/1/19
+ * Time: 9:38
+ */
+
+namespace SimpleDB\Exception;
+
+
+class MySQLException extends \Exception
+{
+	
+}
